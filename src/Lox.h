efce76ee448321d4fbc7a4702d251b2f80c7a0fb
kushal <kushalmondal93@gmail.com>
2023-04-08 00:04:20 +0530

    add logical op tokens, convert Lox.cpp to header

--- src/Lox.h
+++ src/Lox.h
@@ -6,12 +6,12 @@
 class Lox {
   private:
 
-    void report(int line, std::string where, std::string message) {
+    static void report(int line, std::string where, std::string message) {
       std::cout << "[line " << line << "] Error" + where + ": " + message << std::endl;
       hadError = true;
     }
 
-    void run(std::string source) {
+    static void run(std::string source) {
       std::cout << source << std::endl;
       // TODO: scanner class
       //Scanner scanner;
@@ -19,13 +19,13 @@ class Lox {
     }
 
   public:
-    bool hadError;
+    static bool hadError;
 
-    void error(int line, std::string message) {
+    static void error(int line, std::string message) {
       report(line, "", message);
     }
 
-    void runFile(const char *filePath) {  
+    static void runFile(const char *filePath) {  
       std::string path = filePath;
       std::cout << path << std::endl;
       try {
@@ -50,7 +50,7 @@ class Lox {
       }
     }
 
-    void runPrompt() {
+    static void runPrompt() {
       for(;;) {
         std::cout << "> " ;
         std::string line;
@@ -65,18 +65,17 @@ class Lox {
 };
 
 int main(int argc, char const *argv[]) {
-  Lox lox;
-  lox.hadError = false;
+  Lox::hadError = false;
   
   if (argc > 2) {
     std::cout << "Usage: cpplox [script]" << std::endl;
     exit(64);
   }
   else if (argc == 2) {
-    lox.runFile(argv[1]);
+    Lox::runFile(argv[1]);
   }
   else {
-    lox.runPrompt();
+    Lox::runPrompt();
   }
 
   return 0;
--- src/Scanner.cpp
+++ src/Scanner.cpp
@@ -1,64 +1,109 @@
 #include <iostream>
 #include <vector>
 #include "Token.h"
+#include "Lox.h"
 
 class Scanner {
 
-  private:
+private:
     std::string source;
     std::vector<Token> tokens;
     int line = 0, start = 0, current = 0;
 
     bool isAtEnd() {
-      return current >= source.length();
+        return current >= source.length();
     }
 
     char advance() {
-      return source[current++];
+        return source[current++];
     }
 
     void addToken(TokenType type) {
-      addToken(type, NULL_LITERAL);
+        addToken(type, NULL_LITERAL);
     }
 
     void addToken(TokenType type, Literal literal) {
-      std::string text = source.substr(start, current);
-      Token token(type, text, literal, line);
-      tokens.push_back(token);
+        std::string text = source.substr(start, current);
+        Token token(type, text, literal, line);
+        tokens.push_back(token);
+    }
+
+    bool match(char expected) {
+        if(isAtEnd()) 
+            return false;
+        if(source[current] != expected)
+            return false;
+        current++;
+        return true;
     }
 
     void scanToken() {
-      char c = advance();
-      switch(c) {
-        case '(': addToken(LEFT_PAREN); break;
-        case ')': addToken(RIGHT_PAREN); break;
-        case '{': addToken(LEFT_BRACE); break;
-        case '}': addToken(RIGHT_BRACE); break;
-        case ',': addToken(COMMA); break;
-        case '.': addToken(DOT); break;
-        case '-': addToken(MINUS); break;
-        case '+': addToken(PLUS); break;
-        case ';': addToken(SEMICOLON); break;
-        case '*': addToken(STAR); break; 
-      }
+        char c = advance();
+        switch (c) {
+        case '(':
+            addToken(LEFT_PAREN);
+            break;
+        case ')':
+            addToken(RIGHT_PAREN);
+            break;
+        case '{':
+            addToken(LEFT_BRACE);
+            break;
+        case '}':
+            addToken(RIGHT_BRACE);
+            break;
+        case ',':
+            addToken(COMMA);
+            break;
+        case '.':
+            addToken(DOT);
+            break;
+        case '-':
+            addToken(MINUS);
+            break;
+        case '+':
+            addToken(PLUS);
+            break;
+        case ';':
+            addToken(SEMICOLON);
+            break;
+        case '*':
+            addToken(STAR);
+            break; // to support multiline command we gotta change this
+        case '!':
+            addToken(match('=') ? BANG_EQUAL : BANG);
+            break;
+        case '=':
+            addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+            break;
+        case '>':
+            addToken(match('=') ? GREATER_EQUAL : GREATER);
+            break;
+        case '<':
+            addToken(match('=') ? LESS_EQUAL : LESS);
+            break;
+        default :
+            Lox::error(line, "Unexpected character: "+c);
+            break;    
+        }
+
+
     }
 
-  public:
+public:
     Scanner(std::string source) {
-      source = source;
+        source = source;
     }
 
     std::vector<Token> scanTokens() {
-      while(!isAtEnd()) {
-        start = current;
-        scanToken();
-      }
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
 
-      Token eoflToken(EOFL, "", NULL_LITERAL, line);
-      tokens.push_back(eoflToken);
+        // Token eoflToken(EOFL, "", NULL_LITERAL, line);
+        tokens.push_back(Token(EOFL, "", NULL_LITERAL, line));
 
-      return tokens;
+        return tokens;
     }
-
-
 };
